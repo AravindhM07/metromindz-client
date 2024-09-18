2d9d053b65f152b209c7b8a7615a42235a329b1d
AravindhM07 <115076285+AravindhM07@users.noreply.github.com>
2024-09-18 13:21:19 +0530

    Added title.

--- src/components/auth/LogIn.jsx
+++ src/components/auth/LogIn.jsx
@@ -64,6 +64,7 @@ const Login = () => {
 
     return (
         <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+            <h1 className="absolute md:hidden top-7 text-center w-full font-bold text-2xl text-[#464255]">Task Management Hub</h1>
             <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }} className="relative py-3 sm:max-w-xl sm:mx-auto">
                 <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-sky-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
                 <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20 mx-2 md:mx-0 rounded-md">
